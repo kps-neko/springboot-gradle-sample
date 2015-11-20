e17802eb0295f74b79fa27b736f2ad8ee12af3e4
s-wada <s-wada@keyportsolutions.com>
2015-11-20 23:15:29 +0900

    依存関係の見直し（ちぐはぐなバージョンや重複した依存）、gradle Flywayプラグイン導入

--- src/main/java/sample/WebSecurityConfig.java
+++ src/main/java/sample/WebSecurityConfig.java
@@ -6,15 +6,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import sample.security.MyUserDetailsService;
 
 @Configuration
-@EnableWebMvcSecurity
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //    @Autowired
 //    private DataSource dataSource;
